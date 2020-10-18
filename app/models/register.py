9433d320a1b5186db16056951b880e8c72deb44f
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-10-18 20:32:22 -0300

    Alteração de query, função de salvamento de endereço
    Consertada a query e adicionada a função para salvar endereço do usuario

--- app/models/register.py
+++ app/models/register.py
@@ -1,4 +1,5 @@
 from app.exceptions.registerException import RegisteredUser
+from app.models import address
 import mysql.connector
 
 cnx = mysql.connector.connect(user="root",database="recognition_system")
@@ -9,8 +10,10 @@ def register(new_user):
         valid_username(new_user.get('username'))
         valid_email(new_user.get('email'))
         register_user(new_user)
+        address.register_address(new_user)
         return "Register User"
     except (RegisteredUser,Exception) as e:
+        print(e)
         return "Error"
     
 
@@ -33,7 +36,7 @@ def valid_email(email):
 
 def register_user(new_user):
 
-    query = "INSERT INTO USERS (FISTNAME,LASTNAME,EMAIL,USERNAME,PASSSWORD) VALUES(%s, %s, %s, %s, %s)"
+    query = "INSERT INTO USERS (FISTNAME,LASTNAME,EMAIL,USERNAME,PASSWORD) VALUES(%s, %s, %s, %s, %s)"
 
     
     consult.execute(query,(new_user.get('fistname'), new_user.get('lastname'), new_user.get('email'),new_user.get('username'), new_user.get('password')))
