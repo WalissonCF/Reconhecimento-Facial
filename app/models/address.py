5fa9cccd8ccc55eecd902e0e494b3440e3d185c7
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-10-19 12:22:27 -0300

    refatoração do codigo e colocação de exceptions

--- app/models/address.py
+++ app/models/address.py
@@ -1,14 +1,27 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user="root",database="recognition_system")
+cnx = mysql.connector.connect(user="root", database="recognition_system")
 consult = cnx.cursor()
 
+
 def register_address(address_user):
-    query = "INSERT INTO ADDRESS (STREET,RESIDENCE_NUMBER,NEIGHBORHOOD,CITY,STATE) VALUES(%s, %s, %s, %s, %s)"
+    query = """INSERT INTO ADDRESS (STREET,RESIDENCE_NUMBER,NEIGHBORHOOD,CITY,STATE)
+                VALUES(%s, %s, %s, %s, %s)"""
 
     try:
-        consult.execute(query,(address_user.get('rua'),address_user.get('numeroResidencia'), address_user.get('bairro'), address_user.get('cidade'),address_user.get('estado')))
-        cnx.commit()       
+        consult.execute(
+            query,
+            (
+                address_user.get('street'),
+                address_user.get('number'),
+                address_user.get('neighborhood'),
+                address_user.get('city'),
+                address_user.get('state')
+            )
+        )
+        cnx.commit()
     except Exception as ex:
-        print()
+        cnx.rollback()
+        print(ex)
+        print("eeeeee")
         return "Error"
--- app/models/register.py
+++ app/models/register.py
@@ -2,8 +2,9 @@ from app.exceptions.registerException import RegisteredUser
 from app.models import address
 import mysql.connector
 
-cnx = mysql.connector.connect(user="root",database="recognition_system")
+cnx = mysql.connector.connect(user="root", database="recognition_system")
 consult = cnx.cursor()
+
 
 def register(new_user):
     try:
@@ -12,38 +13,58 @@ def register(new_user):
         register_user(new_user)
         address.register_address(new_user)
         return "Register User"
-    except (RegisteredUser,Exception) as e:
+    except (RegisteredUser, Exception) as e:
         print(e)
         return "Error"
-    
+
 
 def valid_username(username):
-    
-    query = "SELECT USERNAME FROM USERS WHERE username = '%s'"
-    consult.execute(query,username)
-    resul = consult.fetchone()
 
-    if resul != None:
-        raise RegisteredUser('erro')
+    query = """SELECT USERNAME FROM USERS 
+        WHERE username = '%s'"""
+
+    try:
+        consult.execute(query, username)
+        result_query = consult.fetchone()
+    except Exception as ex:
+        print(ex)
+
+    if result_query != None:
+        raise RegisteredUser('Usuario já cadastrado')
+
 
 def valid_email(email):
-    query = "SELECT USERNAME FROM USERS WHERE email = '%s'"
-    consult.execute(query,email)
-    resul = consult.fetchone()
+    query = """SELECT USERNAME FROM USERS 
+                WHERE email = '%s'"""
 
-    if resul != None:
-        raise RegisteredUser('erro')
+    try:
+        consult.execute(
+            query,
+            email
+        )
+        result_query = consult.fetchone()
+    except Exception as ex:
+        print(ex)
+
+    if result_query != None:
+        raise RegisteredUser('Email já registrado, informe outro email!')
+
 
 def register_user(new_user):
-
-    query = "INSERT INTO USERS (FISTNAME,LASTNAME,EMAIL,USERNAME,PASSWORD) VALUES(%s, %s, %s, %s, %s)"
-
-    
-    consult.execute(query,(new_user.get('fistname'), new_user.get('lastname'), new_user.get('email'),new_user.get('username'), new_user.get('password')))
-    cnx.commit()
-
-    
-
-
-    
-    
+    query = """INSERT INTO USERS (FISTNAME,LASTNAME,EMAIL,USERNAME,PASSWORD) 
+                VALUES(%s, %s, %s, %s, %s)"""
+    try:
+        consult.execute(
+            query,
+            (
+                new_user.get('fistname'),
+                new_user.get('lastname'),
+                new_user.get('email'),
+                new_user.get('username'),
+                new_user.get('password')
+            )
+        )
+        cnx.commit()
+    except Exception as ex:
+        print(ex)
+        cnx.rollback()
