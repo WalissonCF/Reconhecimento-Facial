22877e03b79ee06d41ede04eb521cc2c5da52adf
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-10-20 10:40:54 -0300

    criado request para login no sistema

--- app/static/js/login/login.js
+++ app/static/js/login/login.js
@@ -1,19 +1,21 @@
-$(document).ready(function(){
-    $("#content-login").submit(function(e){
-        e.preventDefault()
-        
-        const user = $("#user").val()
-        const password = $("#password").val()
 
-        $.post('http://127.0.0.1:5000/validation',
-        {
-            "user":user,
-            "password":password
-        },
-        function(data,status){
-            
-        })
-        
+function loginUser() {
+    var formdata = new FormData();
+    formdata.append('username', $("#username").val());
+    formdata.append('password', $("#password").val());
+
+    var settings = {
+        "url": "http://127.0.0.1:5000/validCredentials",
+        "method": "POST",
+        "timeout": 0,
+        "processData": false,
+        "mimeType": "multipart/form-data",
+        "contentType": false,
+        "data": formdata
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
     });
+}
 
-});
