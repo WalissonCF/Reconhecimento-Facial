fb4cbce93011017fefd2767a90a4a14edd18c0ec
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-10-23 17:49:15 -0300

    Adicionado metodo que procura id de usuario para vinculação do endereço

--- app/models/address.py
+++ app/models/address.py
@@ -5,13 +5,14 @@ consult = cnx.cursor()
 
 
 def register_address(address_user):
-    query = """INSERT INTO ADDRESS (STREET,RESIDENCE_NUMBER,NEIGHBORHOOD,CITY,STATE)
-                VALUES(%s, %s, %s, %s, %s)"""
-
+    query = """INSERT INTO ADDRESS (ID_USER,STREET,RESIDENCE_NUMBER,NEIGHBORHOOD,CITY,STATE)
+                VALUES(%s,%s, %s, %s, %s, %s)"""
+    id_register_user = search_user(address_user.get('username'))
     try:
         consult.execute(
             query,
             (
+                id_register_user[0],
                 address_user.get('street'),
                 address_user.get('number'),
                 address_user.get('neighborhood'),
@@ -24,3 +25,22 @@ def register_address(address_user):
         cnx.rollback()
         print(ex)
         return "Error"
+
+
+def search_user(username):
+    
+    query = """SELECT id FROM USERS 
+        WHERE username = %s"""
+
+    try:
+        consult.execute(
+            query, 
+            (
+                username,
+            )
+        )
+        result_query = consult.fetchone()
+    except Exception as ex:
+        print(ex)
+
+    return result_query
