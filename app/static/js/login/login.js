84abcc401f03e7e9878e88573e8ee45a07a7e367
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-10-25 13:15:29 -0300

    adicionado alert de login no sistema

--- app/static/js/login/login.js
+++ app/static/js/login/login.js
@@ -17,7 +17,10 @@ function loginUser() {
     $.ajax(settings).done(function (response) {
         callback = JSON.parse(response)
         if(callback.authenticated == true){
+            window.alert('Bem vindo ao Sistema')
             window.location.href= "http://127.0.0.1:5000/index"
+        }else{
+            window.alert('Credenciais não conferem')
         }
     });
 }
