e5b466dccc36ef3f1bc2a0c1647cbf39bcf5bc16
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-11-04 18:15:24 -0300

    adicionada a imagem ao request
    adicionada a imagem ao request para o login do usuario

--- app/static/js/login/login.js
+++ app/static/js/login/login.js
@@ -1,8 +1,10 @@
 
 function loginUser() {
+    var canvas = document.getElementById('snapshot')
     var formdata = new FormData();
     formdata.append('username', $("#username").val());
     formdata.append('password', $("#password").val());
+    formdata.append('image', canvas.toDataURL());
 
     var settings = {
         "url": "http://127.0.0.1:5000/validCredentials",
@@ -18,8 +20,8 @@ function loginUser() {
         callback = JSON.parse(response)
         if(callback.authenticated == true){
             window.alert('Bem vindo ao Sistema')
-            window.location.href= "http://127.0.0.1:5000/index"
-        }else{
+            window.location.href = "http://127.0.0.1:5000/index"
+        } else {
             window.alert('Credenciais não conferem')
         }
     });
