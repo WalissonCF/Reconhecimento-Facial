877a6e5ec3e67b5a2d38b777e7e067fad7e73823
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-11-07 13:43:16 -0300

    Alterada rota de index e adicionada a rota home

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,12 +1,10 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 from app.models import register as rg
-from app.models import login as lg
-from app.models import address
 from json import dumps,dump
-import os
 
 @app.route("/")
+@app.route("/index")
 def index():
     return render_template('index.html')
 
@@ -15,16 +13,10 @@ def index():
 def login():
     return render_template('login.html')
 
-@app.route("/index")
+@app.route("/home")
 def logado():
     return render_template('logado.html')
-
-@app.route("/validCredentials", methods=['POST'])
-def valid_credentials():
-    result = lg.valid_user(request.form)
-    return result
     
-
 
 @app.route("/register")
 def register():
