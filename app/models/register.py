bc6d61ac3dd8357737505a8616d889857589c521
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-11-08 17:08:33 -0300

    Refatoração
    Refatoração e remoçap de Try Excepts

--- app/models/register.py
+++ app/models/register.py
@@ -6,10 +6,10 @@ import mysql.connector
 import json
 
 cnx = mysql.connector.connect(
-    host = app.config['HOST'],
-    user = app.config['USERDB'],
-    database = app.config['DATABASE'],
-    password = app.config['PASSWORDDB']
+    host=app.config['HOST'],
+    user=app.config['USERDB'],
+    database=app.config['DATABASE'],
+    password=app.config['PASSWORDDB']
 )
 
 consult = cnx.cursor()
@@ -20,12 +20,14 @@ def register(new_user):
         valid_username(new_user.get('username'))
         valid_email(new_user.get('email'))
         register_user(new_user)
+        address.register_address(new_user)
+        recognition_face.register_face(new_user)
         cnx.commit()
-        return {"register":True}
+        return {"register": True, "error": 'None'}
     except (RegisteredUser, Exception) as e:
         print(e)
         cnx.rollback()
-        return {"register":False, "error":str(e)}
+        return {"register": False, "error": str(e)}
 
 
 def valid_username(username):
@@ -33,16 +35,13 @@ def valid_username(username):
     query = """SELECT USERNAME FROM USERS 
         WHERE username = %s"""
 
-    try:
-        consult.execute(
-            query, 
-            (
-                username,
-            )
+    consult.execute(
+        query,
+        (
+            username,
         )
-        result_query = consult.fetchone()
-    except Exception as ex:
-        print(ex)
+    )
+    result_query = consult.fetchone()
 
     if result_query != None:
         raise RegisteredUser('Usuario ja cadastrado')
@@ -52,16 +51,13 @@ def valid_email(email):
     query = """SELECT USERNAME FROM USERS 
                 WHERE email = %s"""
 
-    try:
-        consult.execute(
-            query,
-            (
-                email,
-            )
+    consult.execute(
+        query,
+        (
+            email,
         )
-        result_query = consult.fetchone()
-    except Exception as ex:
-        print(ex)
+    )
+    result_query = consult.fetchone()
 
     if result_query != None:
         raise RegisteredUser('Email ja registrado, informe outro email!')
@@ -70,21 +66,15 @@ def valid_email(email):
 def register_user(new_user):
     query = """INSERT INTO USERS (FISTNAME,LASTNAME,EMAIL,USERNAME,PASSWORD) 
                 VALUES(%s, %s, %s, %s, %s)"""
-    try:
-        consult.execute(
-            query,
-            (
-                new_user.get('fistname'),
-                new_user.get('lastname'),
-                new_user.get('email'),
-                new_user.get('username'),
-                encoder(new_user.get('password'))
-            )
+
+    consult.execute(
+        query,
+        (
+            new_user.get('fistname'),
+            new_user.get('lastname'),
+            new_user.get('email'),
+            new_user.get('username'),
+            encoder(new_user.get('password'))
         )
-        cnx.commit()
-        address.register_address(new_user)
-        recognition_face.register_face(new_user)
-    except Exception as ex:
-        print(ex)
-        cnx.rollback()
-        raise Exception('Erro ao cadastrar')
+    )
+    cnx.commit()
